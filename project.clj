ba82d78cd49632f0e4e271dcd4a613030f6636e0
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-12-11 01:30:43 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/yogthos/macchiato-framework/macchiato-env"
   :scm {:name "git"
          :url "https://github.com/macchiato-framework/macchiato-env.git"}
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :clojurescript? true
   :dependencies [[macchiato/fs "0.0.2"]]
   :plugins [[codox "0.6.4"]
