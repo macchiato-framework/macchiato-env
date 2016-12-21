ba493e4c0ff3494572e5045c2b11bc437ec849e5
Ricardo J. Mendez <ricardo@numergent.com>
2016-12-21 18:27:29 +0200

    Bumping version, adding changelog

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Environment variable library for macchiato
 
 See the [macchiato framework](https://github.com/macchiato-framework) for more details
+
+
+# TODO
+
+- Tests
+- Getting configuration from a file
--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
-(defproject macchiato/env "0.0.4"
+(defproject macchiato/env "0.0.5-SNAPSHOT"
   :description "a library for managing environment settings from different sources"
-  :url "https://github.com/yogthos/macchiato-framework/macchiato-env"
+  :url "https://github.com/macchiato-framework/macchiato-env"
   :scm {:name "git"
-         :url "https://github.com/macchiato-framework/macchiato-env.git"}
+        :url  "https://github.com/macchiato-framework/macchiato-env.git"}
   :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+            :url  "http://opensource.org/licenses/MIT"}
   :clojurescript? true
   :dependencies [[macchiato/fs "0.0.3"]]
   :plugins [[codox "0.6.4"]
             [lein-npm "0.6.2"]]
-  :npm {:dependencies [
-                       ]}
+  :npm {:dependencies []}
   :profiles {:dev
              {:dependencies [[org.clojure/clojurescript "1.9.293"]]
-              :plugins [[lein-cljsbuild "1.1.4"]]}})
+              :plugins      [[lein-cljsbuild "1.1.4"]]}})
